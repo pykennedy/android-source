2b5430e688073177d1c462c2f5c723f75187dc31
Peter Kennedy <pykennedy@gmail.com>
2015-10-27 17:40:16 -0700

    Checkpoint 25 complete

--- foundation/25-polymorphism/src/com/bloc/polymorph/Main.java
+++ foundation/25-polymorphism/src/com/bloc/polymorph/Main.java
@@ -6,47 +6,33 @@ public class Main extends Object {
 
 	public static void main(String [] args) {
 
-		Dog dog = new Dog();
-		Cat cat = new Cat();
-		Bird bird = new Bird();
-		Snake snake = new Snake();
-		Tarantula tarantula = new Tarantula();
+		Pet[] pets = new Pet[5];		
+		pets[0] = new Dog();
+		pets[1] = new Cat();
+		pets[2] = new Bird();
+		pets[3] = new Snake();
+		pets[4] = new Tarantula();
 
 		/************************************************
 	 	 *	ASSIGNMENT:
 	 	 *	Replace the operations below by employing polymorphism
 		/************************************************/
 
-		dog.feed();
-		dog.wash();
-		dog.exercise();
-
-		cat.feed();
-		cat.wash();
-		cat.exercise();
-
-		bird.feed();
-		bird.wash();
-		bird.exercise();
-
-		snake.feed();
-		snake.wash();
-		snake.exercise();
-
-		tarantula.feed();
-		tarantula.wash();
-		tarantula.exercise();
+		for(int i = 0; i < pets.length; i++) {
+			pets[i].feed();
+			pets[i].wash();
+			pets[i].exercise();
+		}
 
 		/************************************************
 	 	 *	ASSIGNMENT:
 	 	 *	Replace the operations above by employing polymorphism
 		/************************************************/
-
-		assert dog.isFed() && dog.isWashed() && dog.isExercised() : "Your dog needs a little more attention";
-		assert cat.isFed() && cat.isWashed() && cat.isExercised() : "Your cat needs a little more attention";
-		assert bird.isFed() && bird.isWashed() && bird.isExercised() : "Your bird needs a little more attention";
-		assert snake.isFed() && snake.isWashed() && snake.isExercised() : "Your snake needs a little more attention";
-		assert tarantula.isFed() && tarantula.isWashed() && tarantula.isExercised() : "Your tarantula needs a little more attention";
+		
+		for(int i = 0; i < pets.length; i++) {
+			assert pets[i].isFed() && pets[i].isWashed() && pets[i].isExercised() : "Your " + pets[i].getClass().getSimpleName() +
+																					" needs a little more attention";
+		}
 		
 		System.out.println("/************************/");
 		System.out.println("/*                      */");
