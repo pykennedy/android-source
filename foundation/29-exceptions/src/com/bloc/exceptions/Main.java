9ffee4c96b506f46ba452bd4a092e3fc16d5957b
Peter Kennedy <pykennedy@gmail.com>
2015-10-30 20:41:22 -0700

    Checkpoint 29 complete

--- foundation/29-exceptions/src/com/bloc/exceptions/Main.java
+++ foundation/29-exceptions/src/com/bloc/exceptions/Main.java
@@ -3,9 +3,9 @@ package com.bloc.exceptions;
 /************************************************
  *	ASSIGNMENT:
  *	Catch exceptions thrown by the FunMethods class.
- *	Use as few try…catch statements as necessary.
+ *	Use as few tryâ€¦catch statements as necessary.
  *
- *	However, avoid wrapping the entire method in a try…catch,
+ *	However, avoid wrapping the entire method in a tryâ€¦catch,
  *	this is bad practice.
 /************************************************/
 
@@ -33,14 +33,22 @@ public class Main extends Object {
 	/************************************************/
 	private static final void tryGetMax() {
 		int max = 0;
-		max = FunMethods.getMax((Integer[])null);
+		try {
+			max = FunMethods.getMax((Integer[])null);
+		} catch(IllegalArgumentException e) {
+			// do nothing???
+		}
 		Integer[] numbers = new Integer[50];
 		Random rand = new Random();
 		for (int i = 0; i < 50; i++) {
 			numbers[i] = new Integer(rand.nextInt(500));
 		}
 		numbers[32] = null;
-		max = FunMethods.getMax(numbers);
+		try {
+			max = FunMethods.getMax(numbers);
+		} catch(IllegalStateException e) {
+			// do nothing???
+		}
 		numbers[32] = new Integer(rand.nextInt(500));
 		max = FunMethods.getMax(numbers);
 	}
@@ -50,7 +58,11 @@ public class Main extends Object {
  	 *	Catch thrown exceptions
 	/************************************************/
 	private static final void tryRemove() {
-		FunMethods.remove(null, 2);
+		try {
+			FunMethods.remove(null, 2);
+		} catch(IllegalArgumentException e) {
+			// do nothing???
+		}
 		Object[] someObjects = new Object[12];
 		someObjects[0] = "a string!";
 		someObjects[1] = new Integer(32);
@@ -59,7 +71,11 @@ public class Main extends Object {
 		for (int i = 4; i < someObjects.length; i++) {
 			someObjects[i] = String.valueOf(i);
 		}
-		FunMethods.remove(someObjects, 12);
+		try {
+			FunMethods.remove(someObjects, 12);
+		} catch(IndexOutOfBoundsException e) {
+			// do nothing???
+		}
 		someObjects = FunMethods.remove(someObjects, 3);
 	}
 }
